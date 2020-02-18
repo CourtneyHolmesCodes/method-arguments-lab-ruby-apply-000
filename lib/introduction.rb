46ff79c8ad204084485d39d8e06f9c66ffbd19da
Courtney Holmes <courtneylpayne@gmail.com>
2020-02-18 23:09:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,5 @@
 #lib/introduction_with_language.rb
 
-def introduction(name)
-def l
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
